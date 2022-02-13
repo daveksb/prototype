31962345e6427f6228e554d88a2bba6170d490ca
Dave <akkradet@profilegroup-hrs.com>
2022-02-13 13:58:23 +0700

    initial commit

---
+++ apps/flow-management/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prototype-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'flow-management';
+}
